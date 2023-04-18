998b7dabc02e08c005ebb18598713b2e727e67ce
Jonathan B. Sabuncu <sundfo057@gmail.com>
2023-04-18 10:27:40 -0400

    init

---
+++ src/components/Message.jsx
@@ -0,0 +1,10 @@
+import { useState,useEffect ,ComponentState} from "react"
+
+export default function Message () {
+  const [message,setMessage] = useState('Initial Message')
+  useEffect(()=>{
+  setMessage('Component did Mount')
+  },[])
+
+  return (<h2>{message}</h2>)
+}
